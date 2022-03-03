93d24899118d5bf7f05f6b12098a86ba8f8f9062
Igor <igor.reisleandro@gmail.com>
2022-03-03 14:24:22 -0300

    feat: added animation

--- src/views/components/home/home.tsx
+++ src/views/components/home/home.tsx
@@ -21,11 +21,20 @@ export function Home(){
     useEffect(spotlight, [spotlight])
     
     return (
+        // M0 100 L100 100 M100 100 L100 0 M100 0 L0 0 M0 0 L0 100
         <HomeSection {...ScreenTransition} id='home' title='Ygor Rodrigues | Portfólio'>
             <PresentationSection ref={ref}>
                 <motion.span {...item}>Olá, eu sou</motion.span>
                 <AppealingTitle {...item}>Ygor Rodrigues</AppealingTitle>
-                <AnimatedButton {...item} tabIndex={0} onClick={onClick}>este é meu portfólio</AnimatedButton>
+                <AnimatedButton {...item} tabIndex={0} onClick={onClick}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 100" className='stroke' >
+                        <motion.path
+                            d="M0 0 L300 0 M300 0 L300 100 M300 100 L0 100 M0 100 L0 0"
+                            variants={icon} initial="hidden" animate="visible" transition={transition}>
+                        </motion.path>
+                    </svg>
+                    <span>este é meu portfólio</span>
+                </AnimatedButton>
             </PresentationSection>
         </HomeSection>
     )
@@ -48,6 +57,14 @@ const AnimatedButton = styled(motion.button)(props => ({
     fontWeight: 400,
     textTransform: 'uppercase',
     cursor: 'pointer',
+
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    span: {
+        position: 'absolute'
+    }
 }))
 
 const PresentationSection = styled(motion.section)(props => ({
@@ -75,3 +92,35 @@ const item = hiddenVisibleAnim({
       opacity: 1
     }
 });
+
+const transition = {
+    default: { 
+        duration: 1.8, 
+        repeatDelay: 0.5,
+        ease: "easeInOut",
+        repeat: Infinity,
+        repeatType: 'reverse', 
+    },
+    fill: { 
+        duration: 1.8, 
+        repeatDelay: 1,
+        ease: [1, 0, 0.8, 1],
+        repeat: Infinity,
+        repeatType: 'reverse', 
+    }
+}
+
+const icon = {
+    hidden: {
+        y: 0,
+        opacity: 1,
+        fill: "rgba(255, 255, 255, 0)",
+        pathLength: 0,
+    },
+    visible: {
+        y: 5,
+        opacity: 1,
+        fill: "rgba(255, 255, 255, 1)",
+        pathLength: 1,
+    }
+  }
